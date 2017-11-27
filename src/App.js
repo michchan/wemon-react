0afac6052a6b699b9f9c9efa978128db480f12c0
Michael Chan <me@michchan.com>
2017-11-27 08:17:35 +0800

    Updated version on home page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 import Monitor from './components/Monitor';
 import Home from './components/Home';
 
+const packageConfigs = require('../package.json');
+
 class App extends Component {
 
   render() {
@@ -16,6 +18,7 @@ class App extends Component {
             <Link to={'/'} className="App-header__link">
               <i className="fa fa-4x fa-desktop" aria-hidden="true"></i>
               <h1 className="App-title">WeMon - The monitor app</h1>
+              <p className='App-version'>Version: { packageConfigs.version }</p>
             </Link>
           </header>
 
@@ -23,7 +26,6 @@ class App extends Component {
             <Route exact path='/' component={Home}/>
             {/* <Route path='/monitor/:role/:id' component={Monitor}/> */}
           </Switch>
-
         </div>
       </BrowserRouter>
     );
